261ddf39fa9331ec120974cfcf1fcc949b12455e
Tony Montana <theo.angoulvent95@gmail.com>
2024-10-15 17:10:23 +0200

    init main

--- FRONT/src/app/components/main/main.component.ts
+++ FRONT/src/app/components/main/main.component.ts
@@ -9,6 +9,13 @@ export class MainComponent implements OnInit, OnDestroy {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    window.addEventListener('scroll', () => {
+      const scrollHeight = document.documentElement.scrollHeight;
+      const viewportHeight = window.innerHeight;
+      const value = scrollHeight > viewportHeight ? window.pageYOffset / (scrollHeight - viewportHeight) : 0;
+      document.body.style.setProperty('--scroll', value.toString());
+    });
+  }
   ngOnDestroy(): void {}
 }
