ba7712ab3543d15db64e6841c78c1265dd80c662
ADJE Abed <77735946+Abed-Nego28@users.noreply.github.com>
2024-10-16 14:42:41 +0200

    Add login&Register

--- FRONT/src/app/app-routing.module.ts
+++ FRONT/src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { combineLatest } from 'rxjs';
 import { MainComponent } from './components/main/main.component';
 
+
+
 const routes: Routes = [
-  { component: MainComponent, path: "" }
+  { path: 'login', component: LoginComponent },    
+  { path: 'register', component: RegisterComponent },
+
+  { path: '', component: MainComponent },
+
+  
+
+   // redirection par défaut
+ // { path: '', redirectTo: '/login', pathMatch: 'full' }, 
+
+  // Pour toute autre route inconnue, --> vers /login
+  { path: '**', redirectTo: '/login' } 
 ];
 
 @NgModule({
--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -5,19 +5,31 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { MatIconModule } from '@angular/material/icon'
 import { MainComponent } from './components/main/main.component';
+import { FormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     HeaderComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
