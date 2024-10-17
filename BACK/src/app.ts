6bc9bf278316b53d4173fc0ae18bbd52189fe000
Bulbizar <adrienesteves@icloud.com>
2024-10-17 14:42:58 +0200

    add cron : add new seed every midnight akhy bisous

--- BACK/src/app.ts
+++ BACK/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
+import scheduleDailySeed from "./controller/CronTask"
 
 export const App = express();
 
@@ -9,5 +10,4 @@ App.use(express.json());
 App.use('/users', userRoutes);
 App.use('/auth', authRoutes);
 
-
-
+scheduleDailySeed()
