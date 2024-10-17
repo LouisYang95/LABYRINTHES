23414d9d60e5019ef6accf5b1e21b22501994155
Louis YANG <ylouisyang@gmail.com>
2024-10-17 11:30:11 +0200

    [AUTH] readapting auth response

--- BACK/src/controller/authController.ts
+++ BACK/src/controller/authController.ts
@@ -1,27 +1,10 @@
-import { Express, Request, Response} from 'express';
+import {Request, Response} from 'express';
 import User from "../model/User";
 import bcrypt from "bcrypt";
 
-interface registerRequest extends Request {
-    username: string;
-    password: string;
-}
-
-interface loginRequest extends Request {
-    username: string;
-    password: string;
-}
-
-interface loginResponse extends Response {
-   user: User;
-}
-
-interface registerResponse extends Response {
-    message: string;
-}
-
 export const createUser = async (req: Request, res: Response) => {
     try {
+        console.log(req.body);
         const { username, password } = req.body;
 
         // Vérifier si l'utilisateur existe déjà
@@ -49,6 +32,7 @@ export const createUser = async (req: Request, res: Response) => {
         return res.status(201).json({ message: "User created successfully" });
     } catch (error) {
         console.error("Error creating user:", error);
+        console.log(req.body);
         return res.status(500).json({ message: "Error creating user" });
     }
 };
@@ -65,8 +49,15 @@ export const login = async (req: Request, res: Response) => {
                 username: username,
             },
         });
+
         if (!user) {
             return res.status(400).json({message: "Invalid username or password."});
+        }
+
+        const userInfo = {
+            username : user.get("username"),
+            good_points: user.get("good_points"),
+            bad_points: user.get("bad_points"),
         }
 
         const validPassword = bcrypt.compare(password, <string>user.get("password"));
@@ -74,7 +65,7 @@ export const login = async (req: Request, res: Response) => {
             return res.status(400).json({message: "Invalid username or password."});
         }
 
-        return res.status(200).json({message: "Login successful"});
+        return res.status(200).json({message: "Login successful", user: userInfo});
     } catch (error) {
         console.error("Error during user login :", error);
         res
