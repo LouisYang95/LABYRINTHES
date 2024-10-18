afbe7208e623a5a42602ae1c8eb5ded34f60797a
Louis YANG <ylouisyang@gmail.com>
2024-10-18 10:18:08 +0200

    [MARK] Adding mark feature

--- BACK/src/app.ts
+++ BACK/src/app.ts
@@ -3,10 +3,13 @@ import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
 import shopRoutes from "./routes/shopRoutes";
 import scheduleDailySeed from "./controller/CronTask"
+import markRoutes from "./routes/markRoutes";
+
 export const App = express();
 
 App.use(express.json());
 
+App.use('/mark', markRoutes);
 App.use('/user', userRoutes);
 App.use('/auth', authRoutes);
 App.use('/shop', shopRoutes);
