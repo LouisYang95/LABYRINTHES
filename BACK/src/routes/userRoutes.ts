c6ebf0dffe153358c54f8bf341dee0b9e76eaa55
Louis YANG <ylouisyang@gmail.com>
2024-10-23 11:13:33 +0200

    [BACK] hotfix for swagger

--- BACK/src/routes/userRoutes.ts
+++ BACK/src/routes/userRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import {saveTimer} from "../controller/UserController";
-import {getInventory} from "../controller/inventoryController";
+import { saveTimer } from "../controller/UserController";
+import { getInventory } from "../controller/inventoryController";
 
 const router = express.Router();
 
@@ -52,6 +52,45 @@ const router = express.Router();
  *         description: Requête incorrecte
  *       404:
  *         description: Utilisateur non trouvé
+ */
+
+/**
+ * @swagger
+ * /user/{user_id}/inventory:
+ *   get:
+ *     summary: Récupérer l'inventaire de l'utilisateur
+ *     description: Cette route permet de récupérer l'inventaire d'un utilisateur spécifique.
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         user_id: user_id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de l'utilisateur dont on veut récupérer l'inventaire
+ *     responses:
+ *       200:
+ *         description: Inventaire récupéré avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   item_id:
+ *                     type: integer
+ *                     description: ID de l'objet dans l'inventaire
+ *                   item_name:
+ *                     type: string
+ *                     description: Nom de l'objet
+ *                   quantity:
+ *                     type: integer
+ *                     description: Quantité de l'objet dans l'inventaire
+ *       404:
+ *         description: Utilisateur ou inventaire non trouvé
+ *       500:
+ *         description: Erreur serveur lors de la récupération de l'inventaire
  */
 
 router.post('/:user_id/finish', saveTimer);
