506ae44f7a8c8db642e960f96bcfc9a3335f917c
ADJE Abed <77735946+Abed-Nego28@users.noreply.github.com>
2024-10-25 17:38:47 +0200

    Update compte.component.ts

--- FRONT/src/app/components/compte/compte.component.ts
+++ FRONT/src/app/components/compte/compte.component.ts
@@ -8,31 +8,26 @@ import { CompteService, Player } from 'src/app/core/services/compte.service';
 })
 export class CompteComponent implements OnInit {
   playerName: string | null = '';
-  playerPseudo: string | null = '';
+  playerPseudo: string | null= '';
   playerXP: number = 0;
-  playerLikes: string | null = "0";
-  playerDislikes: string | null = "0";
+  playerLikes: string | null= "0";
+  playerDislikes: string | null= "0";
   errorMessage: string = '';
   giftCurrency: number = 0;
 
   constructor(private compteService: CompteService) { }
 
   ngOnInit(): void {
-    // Récupération des informations du joueur depuis le stockage de session
-    this.playerName = sessionStorage.getItem('username');
-    this.playerPseudo = sessionStorage.getItem('username');
-    this.playerLikes = sessionStorage.getItem('good_points');
-    this.playerDislikes = sessionStorage.getItem('bad_points');
-
-    // Gestion des erreurs lors de la récupération des informations du joueur
-    if (!this.playerName || !this.playerPseudo) {
-      this.errorMessage = 'Impossible de charger les informations du joueur.';
-      console.error('Erreur récupération des données:', this.errorMessage);
-    }
-
-}
-
-  ngOnInit(): void {
-    if (this.cookieService.get("username") === undefined || sessionStorage.getItem("username") === null) this.root.navigateByUrl("/");
+    // Récupérer les données du joueur avec id
+        this.playerName = sessionStorage.getItem('username');
+        this.playerPseudo = sessionStorage.getItem('username');
+        this.playerLikes = sessionStorage.getItem('good_points');
+        this.playerDislikes = sessionStorage.getItem('bad_points');
+      
+      (error: any) => {
+        console.error('Erreur récupération des données:', error);
+        this.errorMessage = 'Impossible de charger les informations du joueur.';
+      }
+    
   }
 }
